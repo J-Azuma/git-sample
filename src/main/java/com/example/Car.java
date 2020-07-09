e154ddbe8222f2bb86eeb22dee99b87780667a77
HyogaIto <hyoga.ito@rakus.co.jp>
2020-07-09 11:36:00 +0900

    add property by CarClass

--- src/main/java/com/example/Car.java
+++ src/main/java/com/example/Car.java
@@ -2,4 +2,37 @@ package com.example;
 
 public class Car {
 
+	private  Integer id;
+	private  Integer speed;
+	private  Integer size;
+	private  String name;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getSpeed() {
+		return speed;
+	}
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+	public Integer getSize() {
+		return size;
+	}
+	public void setSize(Integer size) {
+		this.size = size;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
 }
